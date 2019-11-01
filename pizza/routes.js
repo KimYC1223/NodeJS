894141c13c3617d38a463ba4df1ac62280e6402a
KimYC1223 <kau_esc@naver.com>
2019-11-02 00:15:45 +0900

    robo piza web server update

---
+++ pizza/routes.js
@@ -0,0 +1,12 @@
+module.exports = function (app) {
+  let fs = require('fs')
+
+  app.set('view engine','ejs');
+  app.get('/', (req,res) => {
+    res.render(__dirname+'/HTML/index.ejs')
+  })
+
+  app.get('/signUp', (req,res) => {
+    res.render(__dirname+'/HTML/signUp.ejs')
+  })
+}
---
+++ pizza/server.js
@@ -0,0 +1,32 @@
+var express       = require('express')        // call express
+var app           = express()                  // define our app using express
+var bodyParser    = require('body-parser')
+var http          = require('http')
+var fs            = require('fs')
+var path          = require('path')
+var util          = require('util')
+var os            = require('os')
+var session       = require('express-session')
+var mysql         = require('mysql')
+
+app.use(session({
+ secret: 'PIZZA',
+ resave: true,
+ saveUninitialized: true
+}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })); //
+
+
+
+require('./routes.js')(app);
+
+app.use(express.static(path.join(__dirname, './HTML')));
+
+// Save our port
+var port = process.env.PORT || 8000;
+
+// Start the server and listen on port
+app.listen(port,function(){
+  console.log("Live on port: " + port);
+});
