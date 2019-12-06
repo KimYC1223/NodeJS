eb2a15414fb4df519877ba1b619763bebca08c27
KimYC1223 <kau_esc@naver.com>
2019-12-07 03:27:38 +0900

    Webcam udp sender based on OpenCV

---
+++ NodeJS_test/WebCam/OpenCV_CamTest/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace OpenCV_CamTest
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다.
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SendBtn = new System.Windows.Forms.Button();
+            this.IpBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PortBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // SendBtn
+            // 
+            this.SendBtn.Location = new System.Drawing.Point(127, 233);
+            this.SendBtn.Name = "SendBtn";
+            this.SendBtn.Size = new System.Drawing.Size(86, 29);
+            this.SendBtn.TabIndex = 1;
+            this.SendBtn.Text = "전송";
+            this.SendBtn.UseVisualStyleBackColor = true;
+            this.SendBtn.Click += new System.EventHandler(this.SendBtn_Click);
+            // 
+            // IpBox
+            // 
+            this.IpBox.Location = new System.Drawing.Point(89, 206);
+            this.IpBox.Name = "IpBox";
+            this.IpBox.Size = new System.Drawing.Size(124, 21);
+            this.IpBox.TabIndex = 2;
+            this.IpBox.Text = "127.0.0.1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(11, 209);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "서버 IP 주소";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Info;
+            this.label2.Location = new System.Drawing.Point(11, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "포트번호";
+            // 
+            // PortBox
+            // 
+            this.PortBox.Location = new System.Drawing.Point(82, 238);
+            this.PortBox.Name = "PortBox";
+            this.PortBox.Size = new System.Drawing.Size(42, 21);
+            this.PortBox.TabIndex = 5;
+            this.PortBox.Text = "7777";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.ForeColor = System.Drawing.Color.Aqua;
+            this.label3.Location = new System.Drawing.Point(11, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "웹캠 데이터 UDP 전송기";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.label4.Location = new System.Drawing.Point(11, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "김영찬 2019.12.07";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.label5.Location = new System.Drawing.Point(12, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "연결대기";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(225, 299);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.PortBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IpBox);
+            this.Controls.Add(this.SendBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Webcam Data Sender";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button SendBtn;
+        private System.Windows.Forms.TextBox IpBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PortBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ NodeJS_test/WebCam/OpenCV_CamTest/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using OpenCvSharp;
+
+namespace OpenCV_CamTest
+{
+	public partial class Form1 : Form
+	{
+		IplImage m_cvImg;
+		CvCapture m_cvCap;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			m_cvCap = CvCapture.FromCamera(0);
+			m_cvCap.FrameWidth = 200;
+			m_cvCap.FrameHeight = 150;
+
+			//타이머 설정
+			timer1.Interval = 20;
+			timer1.Enabled = true;
+
+            timer2.Interval = 200;
+            timer2.Enabled = true;
+        }
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			//카메라에서 프레임 가져온다.
+			m_cvImg = m_cvCap.QueryFrame();
+			//IplImage을 비트맵으로 전환
+			pictureBox1.Image = m_cvImg.ToBitmap();
+		}
+
+        private void SendBtn_Click(object sender, EventArgs e) {
+            UdpClient cli = new UdpClient();
+            string serverIp = IpBox.Text;
+            string portStr = PortBox.Text;
+            int port = Int32.Parse(portStr);
+            byte[] datagram = converterDemo(pictureBox1.Image);
+            cli.Send(datagram, datagram.Length, serverIp, port);
+        }
+
+        public static byte[] converterDemo(Image x) {
+            ImageConverter _imageConverter = new ImageConverter();
+            byte[] xColor = (byte[])_imageConverter.ConvertTo(x, typeof(byte[]));
+            return xColor;
+        }
+
+        private void Timer2_Tick_1(object sender, EventArgs e) {
+            SendBtn_Click(sender, e);
+        }
+
+        private void Label3_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
